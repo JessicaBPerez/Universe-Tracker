5a7ae0f7d9d814482a0818b751a8c66a3582de5d
Jessica Perez <jessicaperez@Jessicas-MacBook-Pro.local>
2019-03-21 16:55:56 -0400

    Added bootstrap

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is where your Navbar will go</h1>
+      </div>
+    );
+  }
+}
