560396a6381b54c2150e11b68f3579a69584f1f6
Jessica Perez <jessicaperez@Jessicas-MacBook-Pro-2.local>
2019-03-28 11:34:37 -0400

    Aligned items center in Weather

--- client/src/components/Weather.jsx
+++ client/src/components/Weather.jsx
@@ -54,57 +54,70 @@ class Weather extends Component {
           Weather in Real-Time
         </h1>
         <div
-          className="w3-card-4 w3-margin margin weather justify-content-center"
-          style={{ width: "50%", alignItems: "center" }}
+          style={{
+            width: "100vw",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center"
+          }}
         >
-          <div className="w3-display-container w3-text-white justify-content-center">
-            <img src={atlanta2} alt="Lights" style={{ width: "100%" }} />
-            <div className="w3-xlarge w3-display-bottomleft w3-padding">
-              <h1 className="atlanta">{this.state.weatherCity.name}</h1>
-              <br />
-              <h3 className="atlanta-small">
-                Humidity: {this.state.weatherMain}
-              </h3>
-            </div>
-          </div>
-          <div className="w3-row">
-            <div className="w3-third w3-center">
-              <h3 className="atlanta-small button-margin-all">
-                {" "}
-                {this.state.actualWeatherDay1Description}
-              </h3>
+          <div
+            className="w3-card-4 w3-margin margin weather justify-content-center"
+            style={{ width: "50%" }}
+          >
+            <div className="w3-display-container w3-text-white justify-content-center">
               <img
-                className="border-style"
-                src={`http://openweathermap.org/img/w/${
-                  this.state.actualWeatherDay1Icon
-                }.png`}
-                style={{ width: "80px" }}
+                src={atlanta2}
+                alt="Lights"
+                style={{ width: "100%", margin: "0 auto" }}
               />
+              <div className="w3-xlarge w3-display-bottomleft w3-padding">
+                <h1 className="atlanta">{this.state.weatherCity.name}</h1>
+                <br />
+                <h3 className="atlanta-small">
+                  Humidity: {this.state.weatherMain}
+                </h3>
+              </div>
             </div>
-            <div className="w3-third w3-center">
-              <h3 className="atlanta-small button-margin-all">
-                {this.state.actualweatherDay2Description}
-              </h3>
-              <img
-                className="border-style"
-                src={`http://openweathermap.org/img/w/${
-                  this.state.actualWeatherDay2Icon
-                }.png`}
-                style={{ width: "80px" }}
-              />
-            </div>
-            <div className="w3-third w3-center w3-margin-bottom">
-              <h3 className="atlanta-small button-margin-all">
-                {" "}
-                {this.state.actualWeatherDay3Description}
-              </h3>
-              <img
-                className="border-style"
-                src={`http://openweathermap.org/img/w/${
-                  this.state.actualWeatherDay2Icon
-                }.png`}
-                style={{ width: "80px" }}
-              />
+            <div className="w3-row">
+              <div className="w3-third w3-center">
+                <h3 className="atlanta-small button-margin-all">
+                  {" "}
+                  {this.state.actualWeatherDay1Description}
+                </h3>
+                <img
+                  className="border-style"
+                  src={`http://openweathermap.org/img/w/${
+                    this.state.actualWeatherDay1Icon
+                  }.png`}
+                  style={{ width: "80px" }}
+                />
+              </div>
+              <div className="w3-third w3-center">
+                <h3 className="atlanta-small button-margin-all">
+                  {this.state.actualweatherDay2Description}
+                </h3>
+                <img
+                  className="border-style"
+                  src={`http://openweathermap.org/img/w/${
+                    this.state.actualWeatherDay2Icon
+                  }.png`}
+                  style={{ width: "80px" }}
+                />
+              </div>
+              <div className="w3-third w3-center w3-margin-bottom">
+                <h3 className="atlanta-small button-margin-all">
+                  {" "}
+                  {this.state.actualWeatherDay3Description}
+                </h3>
+                <img
+                  className="border-style"
+                  src={`http://openweathermap.org/img/w/${
+                    this.state.actualWeatherDay2Icon
+                  }.png`}
+                  style={{ width: "80px" }}
+                />
+              </div>
             </div>
           </div>
         </div>
